5ef70d3e0cb64562f67c3d2f30089583bb7b7320
Maria Chira <chiramariageorgiana@gmail.com>
2021-10-01 19:56:50 +0100

    Manipulating DOM Elements with html() and position() - p5.js

--- sketch.js
+++ sketch.js
@@ -1,14 +1,26 @@
+var canvas;
+var h1;
+
+var x = 100;
+var y = 100;
 function setup(){
-    createCanvas(200, 200);
+    canvas = createCanvas(200, 200);
+    canvas.position(400, 500);
     createP("My favorite color is purple.");
-    createElement("h1", "My favorite number is below");
+    // h1 = createElement("h1", "My favorite number is below");
+    h1 = createElement("h1", "Waiting.");
+    //h1.position(400, 600);
 }
 function mousePressed() {
+    h1.html("Now I will show you my favorite number.");
     createP("My favorite number is " + random(0, 10));
 }
 function draw(){
-    background(0);
+    // clear();
+    background(200);
     fill(255, 0, 0);
-    rect(100, 100, 50, 50);
+    rect(x, y, 50, 50);
+    h1.position(x, y);
+    x = x + random(-5, 5);
 }
 
